10414179d3cf349f2ff73db7c0f56a468bc6f194
Isaias Nhantumbo <isaiasnhantumbo54@gmail.com>
2022-02-28 13:52:45 +0200

    add new categories

--- src/utils/categories.ts
+++ src/utils/categories.ts
@@ -2,7 +2,8 @@ export const categories = [
     { key: 'purchases', name: 'Compras', icon: 'shopping-bag', color: '#5636D3' },
     { key: 'food', name: 'Alimentação', icon: 'coffee', color: '#FF872C' },
     { key: 'salary', name: 'Salário', icon: 'dollar-sign', color: '#12A454' },
-    { key: 'car', name: 'Carro', icon: 'crosshair', color: '#E83F5B' },
+    { key: 'freelancer', name: 'Frellas', icon: 'key', color: '#12A454' },
+    { key: 'pc', name: 'Computador', icon: 'hard-drive', color: '#E83F5B' },
     { key: 'leisure', name: 'Lazer', icon: 'heart', color: '#26195C' },
     { key: 'studies', name: 'Estudos', icon: 'book', color: '#9C001A' },
 ]; 
